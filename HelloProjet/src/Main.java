cf5577907c6d08e33fe0ac1b8908e1d50239e6a1
Liao <Liao0908hq@163.com>
2019-12-20 08:25:30 +0800

    提交本地仓库

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -2,12 +2,10 @@ import org.junit.Test;
 
 public class Main {
 
-	private String abcDDD;
-
-	private String abc;
 
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("火狐");
     }
 }
